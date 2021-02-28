aa1110b8c1adfb8493a694595311cb6546215fed
lii-10 <79790057+lii-10@users.noreply.github.com>
2021-02-28 16:49:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const darklass = document.getElementById('I')
-const button = document.getElementById('B')
-
-let light = 1;
-
-function On() {
-    if (light === 0) {
-        darklass.classList.add('dark')
-        button.innerHTML = "&#x263E;"
-        light++
-    } else {
-        darklass.classList.remove('dark')
-        button.innerHTML = "&#x263C;"
-        light--
-    }
-}
 // Register service worker to control making site work offline
 
 if ('serviceWorker' in navigator) {
